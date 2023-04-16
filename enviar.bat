e308f6104191d1f6cc913b3494857c026ca8b456
profjosedaniel <daniel.filho@ifma.edu.br>
2023-04-15 23:08:46 -0300

    asd

--- enviar.bat
+++ enviar.bat
@@ -1,11 +1,13 @@
 cd "C:\Users\Daniel\OneDrive - acad.ifma.edu.br\codigos\profjosedaniel\site\"
 
+date /t > .\data.txt
+
 git add *
 
 git commit -m "asd"
 
 git push -u origin main
 
-Get-Date -UFormat %s > .\data.txt
+
 
 exit
